c7605e9c6f81a38627ba19d174e0c285e035fee6
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-10-10 16:50:51 -0300

    Added new migrations and add BayId to detail page

--- Ordos.DataService/Migrations/20181010193729_AddBayId.Designer.cs
+++ Ordos.DataService/Migrations/20181010193729_AddBayId.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ordos.DataService.Data;
 
 namespace Ordos.DataService.Migrations
 {
     [DbContext(typeof(SystemContext))]
-    partial class SystemContextModelSnapshot : ModelSnapshot
+    [Migration("20181010193729_AddBayId")]
+    partial class AddBayId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Ordos.DataService/Migrations/20181010193729_AddBayId.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ordos.DataService.Migrations
+{
+    public partial class AddBayId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BayId",
+                table: "Devices",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BayId",
+                table: "Devices");
+        }
+    }
+}
