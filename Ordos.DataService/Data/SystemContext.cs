02b3fe77e6fa4ab2dc8fb789ae7690c2a7d802f2
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-11-20 01:01:54 -0300

    Working for both Windows and Docker.

--- Ordos.DataService/Data/SystemContext.cs
+++ Ordos.DataService/Data/SystemContext.cs
@@ -7,22 +7,9 @@ namespace Ordos.DataService.Data
 {
     public class SystemContext : DbContext
     {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-        public static string PSQLConnectionString
-        {
-            get
-            {
-                var config = new ConfigurationBuilder()
-                    .SetBasePath(System.AppContext.BaseDirectory)
-                    .AddJsonFile("appsettings.json")
-                    .Build();
+        public static string PSQLConnectionString => Environment.GetEnvironmentVariable("ORDOS_CONNECTION_STRING");
 
-                return config["ConnectionStrings:PostgreSQLConnectionString"];
-            }
-        }
-
-
-        public static Guid guid = new Guid();
+        //public static Guid guid = new Guid();
 
         public SystemContext()
         {
