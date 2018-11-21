0f8c255dc7beed34b5d377ab1038446942546120
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-11-21 18:42:48 -0300

    Add test to check dependecy validation. #46

---
+++ Ordos.Tests/IEC61850DependenciesTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IEC61850.Common;
+using Xunit;
+
+namespace Ordos.Tests
+{
+    public class IEC61850DependenciesTests
+    {
+        [Fact]
+        public void TestCreateMMSValue()
+        {
+            var mmsValue = new MmsValue(true);
+            Assert.NotNull(mmsValue);
+        }
+
+        [Fact]
+        public void TestGetMMSType()
+        {
+            var expected = MmsType.MMS_BOOLEAN;
+            var mmsValue = new MmsValue(true);
+
+            var actual = mmsValue.GetType();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
